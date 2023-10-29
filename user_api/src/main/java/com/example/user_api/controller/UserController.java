07cf127b1c537bd1c67fab1901ec400ccbc0f513
tung1099 <dtroyal1102@gmail.com>
2023-10-29 15:40:56 +0700

    Add activemq

--- user_api/src/main/java/com/example/user_api/controller/UserController.java
+++ user_api/src/main/java/com/example/user_api/controller/UserController.java
@@ -1,11 +1,13 @@
 package com.example.user_api.controller;
 
+import com.example.common.config.avtivemq.JmsConfig;
 import com.example.user_api.model.User;
 import com.example.user_api.repository.user.UserRepositoryImpl;
 import com.example.user_api.service.UserServiceImpl;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.jms.core.JmsTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,16 +27,20 @@ public class UserController {
     @Autowired
     UserServiceImpl userService;
 
+    @Autowired
+    JmsTemplate jmsTemplate;
+
     @GetMapping
     public ResponseEntity<?> findAll(){
-        log.info("\n---------------------------------------------------------------------------&gt;\n");
+        log.info("\n--------------------------------------------------------------------------->\n");
         log.info("[GET] ['{}']", ServletUriComponentsBuilder.fromCurrentRequest().build());
         List<User> users = userRepository.findAll();
+        jmsTemplate.convertAndSend("user_find_all_queue", "Find all users");
         return ResponseEntity.ok(users);
     }
     @GetMapping("/{id}")
     public ResponseEntity<?> findById(@PathVariable String id){
-        log.info("\n---------------------------------------------------------------------------&gt;\n");
+        log.info("\n--------------------------------------------------------------------------->\n");
         log.info("[GET] ['{}']", ServletUriComponentsBuilder.fromCurrentRequest().build());
         Optional<User> users = userService.findById(id);
         return ResponseEntity.ok(users);

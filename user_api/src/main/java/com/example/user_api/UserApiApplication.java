ef7646d3a0b17db28fe54ae7047c5c25edae9679
tung1099 <dtroyal1102@gmail.com>
2023-11-15 19:47:13 +0700

    ADD Swagger

--- user_api/src/main/java/com/example/user_api/UserApiApplication.java
+++ user_api/src/main/java/com/example/user_api/UserApiApplication.java
@@ -3,9 +3,11 @@ package com.example.user_api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @ComponentScan({"com.example.common.*", "com.example.user_api.*"})
+@EnableSwagger2
 public class UserApiApplication {
 
     public static void main(String[] args) {

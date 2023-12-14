b572540d57594c193ed4c610a88adf42d3175c33
tung1099 <dtroyal1102@gmail.com>
2023-12-14 22:31:27 +0700

    refactor

--- user_api/src/main/java/com/example/user_api/repository/IProductRepository.java
+++ user_api/src/main/java/com/example/user_api/repository/IProductRepository.java
@@ -1,9 +1,9 @@
-package com.example.user_api.repository;
-
-
-import com.example.user_api.model.Product;
-import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-
-public interface IProductRepository extends ElasticsearchRepository<Product,Integer> {
-
-}
+//package com.example.user_api.repository;
+//
+//
+//import com.example.user_api.model.Product;
+//import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+//
+//public interface IProductRepository extends ElasticsearchRepository<Product,Integer> {
+//
+//}
--- user_api/src/main/java/com/example/user_api/service/ProductServiceImpl.java
+++ user_api/src/main/java/com/example/user_api/service/ProductServiceImpl.java
@@ -1,31 +1,31 @@
-package com.example.user_api.service;
-
-import com.example.common.config.elasticsearch.ElasticsearchAsync;
-import com.example.user_api.model.Product;
-import com.example.user_api.repository.IProductRepository;
-import org.elasticsearch.action.index.IndexRequest;
-import org.elasticsearch.client.RequestOptions;
-import org.elasticsearch.client.RestHighLevelClient;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import java.util.HashMap;
-import java.util.Map;
-
-@Service
-public class ProductServiceImpl implements IProductService{
-    @Autowired
-    private IProductRepository productRepository;
-
-    @Autowired
-    RestHighLevelClient restHighLevelClient;
-
-    @Override
-    public Iterable<Product> getProducts() {
-        return productRepository.findAll();
-    }
-
-    public Product insertProduct(Product product) {
-        return productRepository.save(product);
-    }
-}
+//package com.example.user_api.service;
+//
+//import com.example.common.config.elasticsearch.ElasticsearchAsync;
+//import com.example.user_api.model.Product;
+//import com.example.user_api.repository.IProductRepository;
+//import org.elasticsearch.action.index.IndexRequest;
+//import org.elasticsearch.client.RequestOptions;
+//import org.elasticsearch.client.RestHighLevelClient;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Service;
+//
+//import java.util.HashMap;
+//import java.util.Map;
+//
+//@Service
+//public class ProductServiceImpl implements IProductService{
+//    @Autowired
+//    private IProductRepository productRepository;
+//
+//    @Autowired
+//    RestHighLevelClient restHighLevelClient;
+//
+//    @Override
+//    public Iterable<Product> getProducts() {
+//        return productRepository.findAll();
+//    }
+//
+//    public Product insertProduct(Product product) {
+//        return productRepository.save(product);
+//    }
+//}
